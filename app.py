d2414d8741134ffecceb3ae0496a9a5ff1b618f3
root <root@kali.viktor>
2020-02-16 10:10:30 -0600

    ADD: articles routing

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-   return render_template('index.html')
+    return render_template('index.html')
+
+@app.route('/articles')
+def articles():
+	return render_template('articles.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
