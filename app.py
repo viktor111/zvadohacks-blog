14240100e3887322894f4f8a49390bdc25221fd9
root <root@kali.viktor>
2020-02-17 07:07:22 -0600

    Adding static files

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path = '/static')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title = 'index')	
 
 @app.route('/articles')
 def articles():
-	return render_template('articles.html')
+	return render_template('articles.html', title = 'articles')
 
 if __name__ == '__main__':
     app.run(debug=True)
