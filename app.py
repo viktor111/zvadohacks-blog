a526857822401a020e87c914894b5bb04751da8f
Viktor <swifrorlilko@gmail.com>
2020-02-20 18:55:34 +0200

    new bracnh

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__, static_url_path = '/static')
 
+
 @app.route('/')
 def index():
-    return render_template('index.html', title = 'index')	
+    return render_template('index.html')
+
 
 @app.route('/articles')
 def articles():
-	return render_template('articles.html', title = 'articles')
+    return render_template('articles.html', title='articles')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
